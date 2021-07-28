55a3af46d42d3e8a4972d89ea5e94ece521d298b
hovanduong <52810802+hovanduong@users.noreply.github.com>
2021-07-28 10:29:00 +0700

    push code node js

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express=require('express');
+const mongoDB=require('./src/database/mongodb/index');
+// const shared=require('./src/shared');
+const cors = require('cors');
+
+var bodyParser=require('body-parser');
+const app=express();
+app.use(express.json());
+app.use(cors());
+const server=require('http').createServer(app);
+
+var jsonParser=bodyParser.json();
+app.use(jsonParser);
+app.use('/',require('./src/routes'));
+server.listen('8081',()=>{
+console.log('Listennig on port 8081');
+    mongoDB.connect();
+});
---
+++ src/database/mongodb/index.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+const mongoUri=process.env.MONGO_CONNECTION_STRING;
+module.exports={
+    async connect(){
+        try {
+            await mongoose.connect(mongoUri,{
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true
+            });
+            console.log('Connected to MongoDB');
+        } catch (error) {
+            console.error("Authentication failed for MongoDB");
+        }
+    }
+}
